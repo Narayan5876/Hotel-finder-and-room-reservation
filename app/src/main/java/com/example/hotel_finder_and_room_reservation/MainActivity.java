564d242a5c615c04e6edaab64694d672958e6ec2
Narayan5876 <naran5876@gmail.com>
2019-11-27 09:36:07 +0545

    My final application Hotel-finder-and-room-reservation

---
+++ app/src/main/java/com/example/hotel_finder_and_room_reservation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.hotel_finder_and_room_reservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
